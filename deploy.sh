383b6152a42cbed80deef22510d1d39d9948e09f
seveniu <prime3721@gmail.com>
2017-12-14 11:51:38 +0800

    配置新的样式

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DIR=/data/nginx/blog   # 此处为你在vps上存放站点的绝对路径
 rm -rf public/
-hugo -b https://blog.seveniu.com && rsync -avz --delete public/ ali:${DIR}
+hugo -b https://blog.seveniu.com/ && rsync -avz --delete public/ ali:${DIR}
 
 exit 0
